bac0ddeb34101ff0e7b3d7d076c7f814bd31959d
hugosenari <hugosenari@gmail.com>
2022-07-21 22:17:34 -0300

    chore: re index asyncthreadpool

---
+++ main/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = ''Awaitable threadpool'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-asyncthreadpool-main.flake = false;
+  inputs.src-asyncthreadpool-main.ref   = "refs/heads/main";
+  inputs.src-asyncthreadpool-main.owner = "yglukhov";
+  inputs.src-asyncthreadpool-main.repo  = "asyncthreadpool";
+  inputs.src-asyncthreadpool-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-asyncthreadpool-main"];
+    over = if builtins.pathExists ./override.nix 
+           then { override = import ./override.nix; }
+           else { };
+  in lib.mkRefOutput (over // {
+    inherit self nixpkgs ;
+    src  = deps."src-asyncthreadpool-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  } );
+}
